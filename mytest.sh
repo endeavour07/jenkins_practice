6ca1c8b176a23f729e131ea23cf3f4441236e277
endeavour07 <125975213+endeavour07@users.noreply.github.com>
2024-11-15 20:46:26 +0530

    Update mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -2,7 +2,7 @@ echo "The first file to excecute in the jenkins"
 
 echo "The build occurs with poll scm"
 
-echo "This build occurs automatically runs"
+echo "This build occurs automatically runs did a change"
 
 date
 
